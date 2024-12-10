b19a2fafd7d4bac06f2ea8bdc8dbe7ff7b5bd371
overcodeme <placmabadei1985@rambler.ru>
2024-12-10 22:39:27 +0300

    Add creating new game button

--- allFunctions.js
+++ allFunctions.js
@@ -179,3 +179,28 @@ export function hasMultipleSolutions(board) {
     solve(board);
     return solutionsCount > 1; 
 }
+
+
+export function sudokuFilling(board, board_field) {
+    
+    for (let blockRow = 0; blockRow < 3; blockRow++) {
+        for (let blockCol = 0; blockCol < 3; blockCol++) {
+            const block = document.createElement('div');
+            block.classList.add('block');
+    
+            for (let rowInBlock = 0; rowInBlock < 3; rowInBlock++) {
+                for (let colInBlock = 0; colInBlock < 3; colInBlock++) {
+                    const elem = document.createElement('div');
+                    elem.classList.add("elem");
+    
+                    if (board[blockRow * 3 + rowInBlock][blockCol * 3 + colInBlock] != 0) {
+                        elem.textContent = board[blockRow * 3 + rowInBlock][blockCol * 3 + colInBlock];
+                    }
+    
+                    block.appendChild(elem);
+                }
+            }
+            board_field.appendChild(block);
+        }
+    }
+}
--- main.js
+++ main.js
@@ -1,36 +1,23 @@
+import { sudokuFilling } from './allFunctions.js';
 import { sudokuGenerator } from './generator.js';
 
 
 let board = sudokuGenerator();
-
-
 const board_field = document.querySelector('.grid-field')
 
-
-for (let blockRow = 0; blockRow < 3; blockRow++) {
-    for (let blockCol = 0; blockCol < 3; blockCol++) {
-        const block = document.createElement('div');
-        block.classList.add('block');
-
-        for (let rowInBlock = 0; rowInBlock < 3; rowInBlock++) {
-            for (let colInBlock = 0; colInBlock < 3; colInBlock++) {
-                const elem = document.createElement('div');
-                elem.classList.add("elem");
-
-                if (board[blockRow * 3 + rowInBlock][blockCol * 3 + colInBlock] != 0) {
-                    elem.textContent = board[blockRow * 3 + rowInBlock][blockCol * 3 + colInBlock];
-                }
-
-                block.appendChild(elem);
-            }
-        }
-        board_field.appendChild(block);
-    }
-}
+sudokuFilling(board, board_field);
 
 let elems = document.querySelectorAll('.elem')
 elems.forEach(() => {
     document.addEventListener('click', () => {
         
     })
+})
+
+
+const new_game_button = document.querySelector('.button1');
+new_game_button.addEventListener('click', () => {
+    board = sudokuGenerator();
+    board_field.innerHTML = '';
+    sudokuFilling(board, board_field);
 })
